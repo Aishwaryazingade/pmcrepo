88efef0ca16a0e72dc30cb1df73b1c0684edde7d
Aishwaryazingade <130381745+Aishwaryazingade@users.noreply.github.com>
2025-09-18 22:06:18 +0530

    Update dummy.java

--- DummyFloder/dummy.java
+++ DummyFloder/dummy.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // This line prints "Hello, World!" to the console
-        System.out.println("Hello, World! ....");
+        for(int i=0;i<12;i++){
+            System.out.println("Hello, World! ...." +i);
+        }
     }
 }
