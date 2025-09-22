58602a6c1deb2370e25986a5d3d77edbfa6641b2
aishwaryazingade <aishwaryazingade292@gmail.com>
2025-09-22 17:33:11 +0530

    1st commit to dummy from loacal

--- DummyFloder/dummy.java
+++ DummyFloder/dummy.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // This line prints "Hello, World!" to the console
         for(int i=0;i<12;i++){
-            System.out.println("Hello, World! ...." +i);
+            System.out.println("Hello, Aishwarya Zingade! ...." +i);
         }
     }
 }
